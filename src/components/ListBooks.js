b912ab55fb644c67119c092822ffae973c2e4274
Kadir Armagan <kadirarmagan@gmail.com>
2021-11-21 10:59:09 +0000

    feat: Add search button

--- src/components/ListBooks.js
+++ src/components/ListBooks.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import BookShelf from "./BookShelf";
 
 function ListBooks() {
@@ -181,9 +182,9 @@ function ListBooks() {
         </div>
       </div>
       <div className="open-search">
-        <button onClick={() => this.setState({ showSearchPage: true })}>
-          Add a book
-        </button>
+        <Link to="/search">
+          <button>Add a book</button>
+        </Link>
       </div>
     </div>
   );
