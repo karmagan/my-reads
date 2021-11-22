37f31518047594058fbcca1c11c7382db8365f91
Kadir Armagan <kadirarmagan@gmail.com>
2021-11-22 09:52:50 +0000

    feat: No books message when no books in a shelf

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -3,24 +3,28 @@ import Book from "./Book";
 
 function BookShelf(props) {
   const { books, shelfName, changeShelf } = props;
+  console.log(books)
   return (
     <div className="bookshelf">
       <h2 className="bookshelf-title">{shelfName}</h2>
-      <div className="bookshelf-books">
-        <ol className="books-grid">
-          {books.map((book) => (
-            <li key={book.id}>
-              <Book
-                title={book.title}
-                authors={book.authors}
-                shelf={book.shelf}
-                imageLinks={book.imageLinks}
-                changeShelf={(shelf)=>changeShelf(book,shelf)}
-              />
-            </li>
-          ))}
-        </ol>
-      </div>
+      {books.length === 0 
+      ? (<p>No books found.</p> )
+      : (<div className="bookshelf-books">
+          <ol className="books-grid">
+            {books.map((book) => (
+              <li key={book.id}>
+                <Book
+                  title={book.title}
+                  authors={book.authors}
+                  shelf={book.shelf}
+                  imageLinks={book.imageLinks}
+                  changeShelf={(shelf) => changeShelf(book, shelf)}
+                />
+              </li>
+            ))}
+          </ol>
+        </div>
+      )}
     </div>
   );
 }
